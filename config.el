58de005931e0c675e29c2db9614f0f13d623e362
Allister Isaiah Harvey <a.i.harvey@icloud.com>
2022-06-21 05:35:02 -0400

    Add Doom Modeline config

--- config.el
+++ config.el
@@ -135,3 +135,74 @@
 (use-package doom-modeline
 :ensure t
 :hook (after-init . doom-modeline-mode))
+
+(setq doom-modeline-support-imenu t)
+
+(setq doom-modeline-height 25)
+
+(setq doom-modeline-bar-width 4)
+
+(setq doom-modeline-hud nil)
+
+(setq doom-modeline-window-width-limit 0.25)
+
+(setq doom-modeline-project-detection 'auto)
+
+(setq doom-modeline-buffer-file-name-style 'auto)
+
+(setq doom-modeline-icon t)
+
+(setq doom-modeline-major-mode-icon t)
+
+(setq doom-modeline-major-mode-color-icon t)
+
+(setq doom-modeline-buffer-state-icon t)
+
+(setq doom-modeline-buffer-modification-icon t)
+
+(setq doom-modeline-unicode-fallback nil)
+
+(setq doom-modeline-buffer-name t)
+
+(setq doom-modeline-minor-modes nil)
+
+(setq doom-modeline-enable-word-count nil)
+
+(setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
+
+(setq doom-modeline-buffer-encoding t)
+
+(setq doom-modeline-indent-info nil)
+
+(setq doom-modeline-checker-simple-format t)
+
+(setq doom-modeline-number-limit 99)
+
+(setq doom-modeline-vcs-max-length 12)
+
+(setq doom-modeline-workspace-name t)
+
+(setq doom-modeline-gnus-timer 2)
+
+(setq doom-modeline-gnus-excluded-groups '("dummy.group"))
+
+(setq doom-modeline-irc t)
+
+(setq doom-modeline-irc-stylize 'identity)
+
+(setq doom-modeline-env-version t)
+
+(setq doom-modeline-env-enable-python t)
+(setq doom-modeline-env-enable-ruby t)
+(setq doom-modeline-env-enable-perl t)
+(setq doom-modeline-env-enable-go t)
+(setq doom-modeline-env-enable-elixir t)
+(setq doom-modeline-env-enable-rust t)
+
+(setq doom-modeline-env-python-executable "python") ; or `python-shell-interpreter'
+(setq doom-modeline-env-rust-executable "rustc")
+
+(setq doom-modeline-env-load-string "...")
+
+(setq doom-modeline-before-update-env-hook nil)
+(setq doom-modeline-after-update-env-hook nil)
