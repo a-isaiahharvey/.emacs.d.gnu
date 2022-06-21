92c3ae8c61b181799a2f0d844884e870798d65b4
Allister Isaiah Harvey <a.i.harvey@icloud.com>
2022-06-20 20:10:10 -0400

    Add Company

--- config.el
+++ config.el
@@ -112,3 +112,15 @@
 (lsp-ui-peek-always-show t)
 (lsp-ui-sideline-show-hover t)
 (lsp-ui-doc-enable nil))
+
+(use-package company
+:ensure
+:custom
+(company-idle-delay 0.5) ;; how long to wait until popup
+;; (company-begin-commands nil) ;; uncomment to disable popup
+:bind
+(:map company-active-map
+	    ("C-n". company-select-next)
+	    ("C-p". company-select-previous)
+	    ("M-<". company-select-first)
+	    ("M->". company-select-last)))
